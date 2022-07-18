117ebf3ace9f8a287f21d5fdb173fe5256819155
Lanestelle <98430918+Lanestelle@users.noreply.github.com>
2022-07-18 19:55:26 -0400

    added one call api integration

--- src/index.js
+++ src/index.js
@@ -30,7 +30,7 @@ function formatDay(timestamp) {
   return days[day];
 }
 
-function displayForecast() {
+function displayForecast(response) {
   let forecastElement = document.querySelector("#forecast");
 
   let forecastHTML = `<div class="row">`;
@@ -53,6 +53,12 @@ function displayForecast() {
   forecastElement.innerHTML = forecastHTML;
 }
 
+function getForecast(coordinates) {
+  apiKey = "24e44405ec4458d131d2eebd";
+  apiUrl = `https://api.openweathermap.org/data/3.0/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}`;
+  axios.get(apiUrl).then(displayForecast);
+}
+
 function displayTemperature(response) {
   let city = document.querySelector("#city");
   city.innerHTML = response.data.name;
@@ -73,10 +79,11 @@ function displayTemperature(response) {
   );
   iconElement.setAttribute("alt", response.data.weather[0].description);
   fahrenheitTemperature = response.data.main.temp;
+  getForecast(response.data.coord);
 }
 function search(city) {
   let apiKey = "766f9bb424e44405ec4458d131d2eebd";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}&units=imperial`;
   axios.get(apiUrl).then(displayTemperature);
 }
 
